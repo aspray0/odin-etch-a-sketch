76790186c08521cf21990321e92b4b26f3cb272f
aspray0 <andrew.d.spray@gmail.com>
2022-11-19 13:23:59 -0800

    Pseudocode makeDivs function

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // script.js
+
+// make the grid default to 16x16
+
+// store container div inside container variable
+
+// make a function called makeDivs with num as a parameter
+// that runs num times
+// and makes a vert div each time
+// adds class vert to each div
+//  within that div, run another loop num times
+//  that makes a horizontal div each time
+//  adds hori class to each div
+//  and appends those divs to the vert div
+// and appends that div to container
