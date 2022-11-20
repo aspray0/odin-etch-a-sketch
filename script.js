62fc882dae59cfe6f66e0d715c457bf458e91a35
aspray0 <andrew.d.spray@gmail.com>
2022-11-19 16:33:16 -0800

    Add class declarations to makeDivs

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 // script.js
 
-let num = 16;// make the grid default to 16x16
+let num = 16; // make the grid default to 16x16
 
 const container = document.querySelector('#container'); // store container div inside container variable
 
 function makeDivs(num) { // make a function called makeDivs with num as a parameter
   for (let i = 0; i < num; i++) { // that runs num times
-    let vertDiv = document.createElement('div');// and makes a vert div each time
+    let vertDiv = document.createElement('div'); // and makes a vert div each time
+    vertDiv.classList.add('vert'); // add class vert to vertDiv
     for (let j = 0; j < num; j++) { //  within that div, run another loop num times
-      let horiDiv = document.createElement('div');//  that makes a horizontal div each time
-      vertDiv.appendChild(horiDiv);//  and appends those divs to the vert div
+      let horiDiv = document.createElement('div'); //  that makes a horizontal div each time
+      horiDiv.classList.add('hori'); // add class hori to horiDiv
+      vertDiv.appendChild(horiDiv); //  and appends those divs to the vert div
     }
-    container.appendChild(vertDiv);// and appends that div to container
+    container.appendChild(vertDiv); // and appends that div to container
   }
 }
