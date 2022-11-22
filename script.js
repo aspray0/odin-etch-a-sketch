d7c53e9c425c6606478827e7328e3720cdf55dfc
aspray0 <andrew.d.spray@gmail.com>
2022-11-21 17:48:38 -0800

    Translate resetGrid, apply button listener call

--- script.js
+++ script.js
@@ -2,16 +2,21 @@
 
 let num = 16; // make the grid default to 16x16
 
-// make function resetGrid
-// which prompts the user for a number between 1 and 100
-// if entry is not a number between 1 and 100
-// alert that number needs to be between 1 and 100
-// else
-// clear horiDivs and vertDivs
-// call makeDivs function w/ user input as parameter
+function resetGrid() { // make function resetGrid
+  num = Number(prompt('Please enter the number of rows and columns for the new grid.'));// which prompts the user for a number between 1 and 100
+  if (!((num >= 1) && (num <= 100))) { // if entry is not a number between 1 and 100
+    alert('Please enter a number greater than or equal to 1 and less than or equal to 100.'); // alert that number needs to be between 1 and 100
+  } else { // else
+    while (container.firstChild) { // clear horiDivs and vertDivs
+      container.removeChild(container.firstChild);
+    };
+    makeDivs(num);// call makeDivs function w/ user input as parameter
+  }
+}
 
-// select button
-// add eventListener for click that runs a function
+const button = document.querySelector('button'); // select button
+
+button.addEventListener("click", resetGrid); // add eventListener for click that runs a function
 
 const container = document.querySelector('#container'); // store container div inside container variable
 
